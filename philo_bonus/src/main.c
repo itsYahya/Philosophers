a99b9713bb1e473e3b870263ed14f4940cb8ccc0
yel-mrab <yahya181elmrabti@gmail.com>
2022-11-18 03:45:06 +0100

    feat: main implementation

--- philo_bonus/src/main.c
+++ philo_bonus/src/main.c
@@ -6,12 +6,13 @@
 /*   By: yel-mrab <yel-mrab@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/03 14:55:58 by yel-mrab          #+#    #+#             */
-/*   Updated: 2022/05/03 17:45:15 by yel-mrab         ###   ########.fr       */
+/*   Updated: 2022/11/18 03:39:12 by yel-mrab         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "philo.h"
+#include "philo_b.h"
 #include <stdlib.h>
+#include <stdio.h>
 
 int	main(int argc, char **argv)
 {
@@ -20,8 +21,15 @@ int	main(int argc, char **argv)
 	if (argc == 5 || argc == 6)
 	{
 		if (ft_init(&data, argv + 1))
-			return (ft_putstr("one of the arguments is not correct\n", 2), 1);
-		
+			return (ft_putstr("ops: one of the arguments isnt correct\n", 2), 1);
+		if (ft_init_sema(&data))
+			return (ft_putstr("ops: failed to initialize semaphores\n", 2), 1);
+		if (ft_starter(&data))
+			return (ft_putstr("ops: error accured\n", 2), 1);
+		waitpid(-1, 0, 0);
+		ft_unlik_sem();
+		ft_kill(&data);
+		free(data.pids);
 		free(data.philos);
 	}
 	return (0);
